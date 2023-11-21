349c7ad8559c1d8b568316631d1fcded0de103e6
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2023-11-21 19:39:52 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/gob"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type Frameit struct {
+	Type   string `json:"type"`
+	Button string `json:"button"`
+	Text   string `json:"text"`
+}
+
+func mdfunc(w http.ResponseWriter, r *http.Request) {
+	htmlText, btns := MD2HTMLButtonsV2(r.FormValue("rtext"))
+	formatedtrext := Frameit{
+		Type:   "success!",
+		Button: fmt.Sprintf("%v", btns),
+		Text:   htmlText,
+	}
+	if err := gob.NewEncoder(w).Encode(formatedtrext); err != nil {
+		log.Print(err.Error())
+	}
+}
+
+func homePage(w http.ResponseWriter, _ *http.Request) {
+	if _, err := fmt.Fprint(w, "Welcome!"); err != nil {
+		log.Println(err.Error())
+	}
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9002"
+	}
+	server := &http.Server{
+		Addr:         "0.0.0.0:" + port,
+		Handler:      router,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 3 * time.Second,
+	}
+	router.HandleFunc("/", homePage)
+	router.HandleFunc("/get/", mdfunc).Methods("POST")
+	log.Println("Started API!")
+	log.Fatal(server.ListenAndServe())
+}
